6c47fa5ca948a12426fc384da9c975b5370d6cc8
Aditya Singh <singhaditya2763@gmail.com>
2022-06-24 21:53:43 +0530

    upadted home1 component

--- components/Home1/home1.js
+++ components/Home1/home1.js
@@ -11,7 +11,7 @@ const Home1 = () => {
           <div className={styles.header_logo}>
             <h3>
            
-              <img src="/Frame.svg" />
+              <img src="/Frame1.png" />
             </h3>
 
             <h3 className={styles.header_log_title}>AIVARA</h3>
