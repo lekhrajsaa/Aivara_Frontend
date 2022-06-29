a601d8b8b6fa4fc70da163fe9a1a8cd3979691ea
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2022-06-29 09:32:53 +0530

    updated_annalysis_steps

--- components/Analysis_details_slider2/step.jsx
+++ components/Analysis_details_slider2/step.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+// import { useSwiper } from 'swiper/react';
 import classes from './step.module.css';
 
 const Step = ({countNum, stepInfo}) => {
-
+    // const swiper = useSwiper()
     return (
         <div className={`${classes.step}`}>
             <span className={classes.countNum}>{countNum}</span>
