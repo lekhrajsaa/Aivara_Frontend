1d6ff7aecc0644fa3c2186b5c9c8529c89c66d2c
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2022-07-19 00:01:13 +0530

    added_form_submission_response_popups

--- components/Analysis-request-form/analysisRequestForm.jsx
+++ components/Analysis-request-form/analysisRequestForm.jsx
@@ -4,6 +4,14 @@ import axios from 'axios';
 import classes from './analysisRequestForm.module.css'
 import Country from "./countries"
 
+import {
+  Dialog,
+  DialogTitle,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 
 const AnalysisRequestForm = () => {
 
@@ -20,6 +28,9 @@ const AnalysisRequestForm = () => {
 
   const [dragDropInputText, setDragDropInputText] = useState('Drag and drop file here');
   const [dragDropInputStyles, setDragDropInputStyles] = useState({ border: "1px dashed rgba(0, 0, 0, 0.5)" });
+
+  const [openAlertBox, setOpenAlertBox] = useState(false);
+  const [openErrorBox, setOpenErrorBox] = useState(false);
 
 
   const AnalysisRequestFormSubmitHandler = async e => {
@@ -46,8 +57,10 @@ const AnalysisRequestForm = () => {
         }
       });
       console.log('response:', response)
+      setOpenAlertBox(true)
     } catch (err) {
       console.log('err: ', err)
+      setOpenErrorBox(true)
     }
 
     setFirstName('');
@@ -120,133 +133,175 @@ const AnalysisRequestForm = () => {
   }
 
   return (
-    <form
-      onSubmit={AnalysisRequestFormSubmitHandler}
-      className={classes.formFields}
-    >
-      <label htmlFor="firstName">First Name</label>
-      <input
-        type="text"
-        id="firstName"
-        minLength="3"
-        required
-        value={firstName}
-        onInput={firstNameInpChangeHandler}
-      />
-
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        type="text"
-        id="lastName"
-        minLength="3"
-        required
-        value={lastName}
-        onInput={lastNameInpChangeHandler}
-      />
-
-      <label htmlFor="jobTitle">Job Title</label>
-      <input
-        type="text"
-        id="jobTitle"
-        required
-        value={jobTitle}
-        onInput={jobTitleInpChangeHandler}
-      />
-
-      <label htmlFor="companyName">Company Name</label>
-      <input
-        type="text"
-        id="companyName"
-        minLength="3"
-        required
-        value={companyName}
-        onInput={companyNameInpChangeHandler}
-      />
-
-      <label htmlFor="emailAddress">Email Address</label>
-      <input
-        type="email"
-        id="emailAddress"
-        required
-        value={emailAddress}
-        onInput={emailAddressInpChangeHandler}
-      />
-
-      <label htmlFor="contactNo">Contact Number</label>
-      <input
-        type="tel"
-        id="contactNo"
-        minLength="10"
-        maxLength="10"
-        required
-        value={contactNumber}
-        onInput={contactNumberChangeHandler}
-      />
-
-      <label htmlFor="country" placeholder="Select your country">
-        Country
-      </label>
-      <Country country={country} setCountry={setCountry} />
-
-      <label htmlFor="application">Application</label>
-      <select
-        name="application"
-        id="application"
-        required
-        onChange={selectApplicationChangeHandler}
+    <>
+      <form
+        onSubmit={AnalysisRequestFormSubmitHandler}
+        className={classes.formFields}
       >
-        <option value=""></option>
-        <option value="Application1" style={{ fontSize: "15px" }}>
-          Application1
-        </option>
-        <option value="Application2" style={{ fontSize: "15px" }}>
-          Application1
-        </option>
-      </select>
-
-      <label htmlFor="attachments">Attachments</label>
-      <label
-        className={classes.file_upload}
-        onDrop={dragDropHandler}
-        onDragOver={dragOverHandler}
-        onDragLeave={dragLeaveHandler}
-        style={{ ...dragDropInputStyles, position: 'relative' }}
-
-      >
-        {dragDropInputText}
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <span className="material-symbols-outlined"
-          style={{
-            position: 'absolute',
-            top: '50%',
-            transform: 'translate(0, -50%) rotate(40deg)',
-            margin: 'auto'
-          }}>
-          attach_file
-        </span>
+        <label htmlFor="firstName">First Name</label>
         <input
-          className={classes.file_upload_input}
-          type="file"
-          id="attachments"
-          accept="image/jpeg,image/gif,image/png,application/pdf,application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint,text/plain,image/x-eps,application/zip,application/rar,application/7zip "
-          name="AttachmentsInput"
-          onChange={attachmentInpChangeHanlder}
+          type="text"
+          id="firstName"
+          minLength="3"
           required
-          multiple
+          value={firstName}
+          onInput={firstNameInpChangeHandler}
         />
-      </label>
-      <label htmlFor="analysisGoal">Sample Description/Analysis Goal</label>
-      <input
-        value={analysisGoal}
-        type="text"
-        htmlFor="analysisGoal"
-        id="analysisGoal"
-        required
-        onInput={analysisGoalChangeHanlder}
-      />
 
-      <button type="submit">Submit</button>
-    </form>
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          minLength="3"
+          required
+          value={lastName}
+          onInput={lastNameInpChangeHandler}
+        />
+
+        <label htmlFor="jobTitle">Job Title</label>
+        <input
+          type="text"
+          id="jobTitle"
+          required
+          value={jobTitle}
+          onInput={jobTitleInpChangeHandler}
+        />
+
+        <label htmlFor="companyName">Company Name</label>
+        <input
+          type="text"
+          id="companyName"
+          minLength="3"
+          required
+          value={companyName}
+          onInput={companyNameInpChangeHandler}
+        />
+
+        <label htmlFor="emailAddress">Email Address</label>
+        <input
+          type="email"
+          id="emailAddress"
+          required
+          value={emailAddress}
+          onInput={emailAddressInpChangeHandler}
+        />
+
+        <label htmlFor="contactNo">Contact Number</label>
+        <input
+          type="tel"
+          id="contactNo"
+          minLength="10"
+          maxLength="10"
+          required
+          value={contactNumber}
+          onInput={contactNumberChangeHandler}
+        />
+
+        <label htmlFor="country" placeholder="Select your country">
+          Country
+        </label>
+        <Country country={country} setCountry={setCountry} />
+
+        <label htmlFor="application">Application</label>
+        <select
+          name="application"
+          id="application"
+          required
+          onChange={selectApplicationChangeHandler}
+        >
+          <option value=""></option>
+          <option value="Application1" style={{ fontSize: "15px" }}>
+            Application1
+          </option>
+          <option value="Application2" style={{ fontSize: "15px" }}>
+            Application1
+          </option>
+        </select>
+
+        <label htmlFor="attachments">Attachments</label>
+        <label
+          className={classes.file_upload}
+          onDrop={dragDropHandler}
+          onDragOver={dragOverHandler}
+          onDragLeave={dragLeaveHandler}
+          style={{ ...dragDropInputStyles, position: 'relative' }}
+
+        >
+          {dragDropInputText}
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="material-symbols-outlined"
+            style={{
+              position: 'absolute',
+              top: '50%',
+              transform: 'translate(0, -50%) rotate(40deg)',
+              margin: 'auto'
+            }}>
+            attach_file
+          </span>
+          <input
+            className={classes.file_upload_input}
+            type="file"
+            id="attachments"
+            accept="image/jpeg,image/gif,image/png,application/pdf,application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint,text/plain,image/x-eps,application/zip,application/rar,application/7zip "
+            name="AttachmentsInput"
+            onChange={attachmentInpChangeHanlder}
+            required
+            multiple
+          />
+        </label>
+        <label htmlFor="analysisGoal">Sample Description/Analysis Goal</label>
+        <input
+          value={analysisGoal}
+          type="text"
+          htmlFor="analysisGoal"
+          id="analysisGoal"
+          required
+          onInput={analysisGoalChangeHanlder}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <Dialog
+        open={openAlertBox}
+        aria-labelledby="dilog-title"
+        aria-aria-describedby="dilog-description"
+        sx={{ p: 2 }}
+      >
+        <DialogTitle id="dilog-title">Succesfull!</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Your Form Has been submitted!</DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ mx: 1, mb: 1 }}>
+          <Button
+            variant="contained"
+            onClick={() => { setOpenAlertBox(false) }}
+          >
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={openErrorBox}
+        aria-labelledby="dilog-title"
+        aria-aria-describedby="dilog-description"
+        sx={{ p: 2 }}
+      >
+        <DialogTitle id="dilog-title">Error!</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Form submission failed!</DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ mx: 1, mb: 1 }}>
+          <Button
+            variant="contained"
+            onClick={() => { setOpenErrorBox(false) }}
+          >
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
--- components/Home2/contactus_form/contactus.jsx
+++ components/Home2/contactus_form/contactus.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import classes from '../home2.module.css'
 
+import {
+    Dialog,
+    DialogTitle,
+    Button,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+} from "@mui/material";
+
+
 const Contactus = () => {
+    const [openAlertBox, setOpenAlertBox] = useState(false);
+    const [openErrorBox, setOpenErrorBox] = useState(false);
+
+    const form = useRef();
 
     async function contactUsFormSubmitHanlder(e) {
         e.preventDefault();
@@ -36,103 +50,152 @@ const Contactus = () => {
             method: "POST",
             headers: {
                 "x-api-key": `${process.env.NEXT_PUBLIC_X_API_KEY}`,
-                
+
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(body),
         }).then(res => res.json())
-        .then(data => console.log('response: ', data))
-        .catch(err => console.log(err))
+            .then(data => {
+                console.log('response: ', data);
+                setOpenAlertBox(true)
+                form.current.reset()
+            })
+            .catch(err => {
+                console.log(err)
+                form.current.reset()
+                setOpenErrorBox(true)
+            })
 
     }
 
     return (
-        <form onSubmit={contactUsFormSubmitHanlder}>
-            <div className={classes.formsAndtextarea}>
-                <div className={classes.form_wrapper}>
+        <>
+            <form ref={form} onSubmit={contactUsFormSubmitHanlder}>
+                <div className={classes.formsAndtextarea}>
+                    <div className={classes.form_wrapper}>
 
-                    {/* newsletter-signup-form-label form-label */}
-                    <div className={classes.newsletter_signup_form_label_wrapper}>
-                        <input
-                            className="form-input"
-                            type="text"
-                            name="companyName"
-                            id="companyName"
-                            required
-                        />
-                        <label
-                            className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
-                            htmlFor="company_name"
-                            id="company_name"
-                        >
-                            Company name
-                        </label>
+                        {/* newsletter-signup-form-label form-label */}
+                        <div className={classes.newsletter_signup_form_label_wrapper}>
+                            <input
+                                className="form-input"
+                                type="text"
+                                name="companyName"
+                                id="companyName"
+                                required
+                            />
+                            <label
+                                className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
+                                htmlFor="company_name"
+                                id="company_name"
+                            >
+                                Company name
+                            </label>
+                        </div>
+                        <div className={classes.newsletter_signup_form_label_wrapper}>
+                            <input
+                                className="form-input"
+                                type="email"
+                                name="companyEmail"
+                                id="email"
+                                required
+                            />
+                            <label
+                                className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
+                                htmlFor="company_email"
+                                id="company_email"
+
+                            >
+                                Company email
+                            </label>
+                        </div>
+                        <div className={classes.newsletter_signup_form_label_wrapper}>
+                            <input
+                                className="form-input"
+                                type="tel"
+                                name="contactNumber"
+                                id="contact_number"
+                                required
+                            />
+                            <label
+                                className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
+                                htmlFor="contact_number"
+                                id="contact_number"
+                            >
+                                Contact number
+                            </label>
+                        </div>
+
                     </div>
-                    <div className={classes.newsletter_signup_form_label_wrapper}>
-                        <input
-                            className="form-input"
-                            type="email"
-                            name="companyEmail"
-                            id="email"
-                            required
-                        />
-                        <label
-                            className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
-                            htmlFor="company_email"
-                            id="company_email"
 
-                        >
-                            Company email
-                        </label>
-                    </div>
-                    <div className={classes.newsletter_signup_form_label_wrapper}>
-                        <input
-                            className="form-input"
-                            type="tel"
-                            name="contactNumber"
-                            id="contact_number"
-                            required
-                        />
-                        <label
-                            className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
-                            htmlFor="contact_number"
-                            id="contact_number"
-                        >
-                            Contact number
-                        </label>
-                    </div>
-
-                </div>
-
-                <div className={classes.textareas}>
-                    {/* <textarea
+                    <div className={classes.textareas}>
+                        {/* <textarea
             placeholder="Message"
             rows="30"
             data-min-rows="30"
           ></textarea> */}
-                    <div className={classes.newsletter_signup_form_label_wrapper}>
-                        <input
-                            className="form-input"
-                            type="text"
-                            name="message"
-                            id="message"
-                            required
-                        />
-                        <label
-                            className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
-                            htmlFor="message"
-                            id="message"
-                            style={{ color: "#395D89",fontSize:" 20px"}}
-                        >
-                            Message
-                        </label>
+                        <div className={classes.newsletter_signup_form_label_wrapper}>
+                            <input
+                                className="form-input"
+                                type="text"
+                                name="message"
+                                id="message"
+                                required
+                            />
+                            <label
+                                className={`${classes.newsletter_signup_form_label} ${classes.from_lable}`}
+                                htmlFor="message"
+                                id="message"
+                                style={{ color: "#395D89", fontSize: " 20px" }}
+                            >
+                                Message
+                            </label>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <button className={classes.submit_btn} type="submit">
-                Submit
-            </button>
-        </form>
+                <button className={classes.submit_btn} type="submit">
+                    Submit
+                </button>
+            </form>
+            <Dialog
+                open={openAlertBox}
+                aria-labelledby="dilog-title"
+                aria-aria-describedby="dilog-description"
+                sx={{ p: 2 }}
+            >
+                <DialogTitle id="dilog-title">Succesfull!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>Your Form Has been submitted!</DialogContentText>
+                </DialogContent>
+                <DialogActions sx={{ mx: 1, mb: 1 }}>
+                    <Button
+                        variant="contained"
+                        onClick={() => { setOpenAlertBox(false) }}
+                    >
+                        Okay
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog
+                open={openErrorBox}
+                aria-labelledby="dilog-title"
+                aria-aria-describedby="dilog-description"
+                sx={{ p: 2 }}
+            >
+                <DialogTitle id="dilog-title">Error!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>Form submission failed!</DialogContentText>
+                </DialogContent>
+                <DialogActions sx={{ mx: 1, mb: 1 }}>
+                    <Button
+                        variant="contained"
+                        onClick={() => { setOpenErrorBox(false) }}
+                    >
+                        Okay
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 }
 
